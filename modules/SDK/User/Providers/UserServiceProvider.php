501754d92622d5f13b8c682cbac9d4124143ada1
sina1010anis <sina1010anis@gmail.com>
2023-09-03 19:11:35 +0330

    module

--- modules/SDK/User/Providers/UserServiceProvider.php
+++ modules/SDK/User/Providers/UserServiceProvider.php
@@ -9,9 +9,9 @@ class UserServiceProvider extends ServiceProvider
 {
     public function register ()
     {
+        $this->loadRoutesFrom(__DIR__. '/../Routes/route_user.php');
         $this->mergeConfigFrom(__DIR__. '/../Config/user_config.php', 'userConfig');
         $this->loadMigrationsFrom(__DIR__. '/../Database/Migrations/');
-        $this->loadRoutesFrom(__DIR__. '/../Routes/route_user.php');
         $this->loadViewsFrom(__DIR__. '/../Resources/Views/', 'User');
         $this->loadViewComponentsAs('user', [
             Btn::class
